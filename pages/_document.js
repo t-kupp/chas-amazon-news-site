59c3e13fd128df0743c27c435b114636b32de351
Jan <kupper.thorge@gmail.com>
2025-02-13 20:15:56 +0100

    fixed (?) horizontal scrollbar showing up

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,10 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html className='w-screen transition-[background-color] duration-100' lang='en'>
+    <Html
+      className='w-screen overflow-x-hidden transition-[background-color] duration-100'
+      lang='en'
+    >
       <Head />
       <body className='antialiased'>
         <Main />
